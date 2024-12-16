3525522b74bc5343bedf64bc0f824a6b54d7e251
Camposx <alejandrocamposgon@gmail.com>
2024-12-16 11:34:42 +0100

    feat: links between views

--- resources/views/santa.blade.php
+++ resources/views/santa.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-    <a href="{{ route('santa') }}"> <img src="{{ config('image.santa')}}" alt="Santa claus"></a>
+    <a href="{{ route('home') }}"> <img src="{{ config('image.xmasTree')}}" alt="Christmas Tree"></a>
     <a href="{{ route('elf') }}"> <img src="{{ config('image.elf')}}" alt="Elf"></a>
 @endsection
