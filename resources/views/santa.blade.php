9fea48966cb683cd28c827f21319a2b8c932926a
Camposx <alejandrocamposgon@gmail.com>
2024-12-16 12:39:15 +0100

    feat: Add children cards in santa view

--- resources/views/santa.blade.php
+++ resources/views/santa.blade.php
@@ -3,4 +3,21 @@
 @section('content')
     <a href="{{ route('home') }}"> <img src="{{ config('image.xmasTree')}}" alt="Christmas Tree"></a>
     <a href="{{ route('elf') }}"> <img src="{{ config('image.elf')}}" alt="Elf"></a>
+
+    @foreach ($children as $child)
+        <div class="card" style="width: 18rem;">
+            <img src="{{$child->photo}}" class="card-img-top" alt="Photo of {{$child->name}} {{$child->surname}}">
+            <div class="card-body">
+                <h5 class="card-title">{{$child->name}} {{$child->surname}}</h5>
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">Age: {{$child->age}}</li>
+                <li class="list-group-item">Behavior: {{$child->naughty}}</li>
+            </ul>
+            <div class="card-body">
+                <a href="#" class="card-link">Gifts</a>
+            </div>
+        </div>
+    @endforeach
+    
 @endsection
