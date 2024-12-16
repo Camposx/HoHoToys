c0a6cf9e01d7ba5f51de3aeab3e806d0ca3636c0
Jaz356 <joziahwalters70@gmail.com>
2024-12-16 11:59:15 +0100

    feat:add api child routes

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ChildController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/',[ChildController::class, 'index'])->name('apiSantaHome');
+Route::get('/santa/{id}',[ChildController::class, 'show'])->name('apiSantaShow');
+Route::delete('/santa/{id}',[ChildController::class, 'destroy'])->name('apiSantaDestroy');
+Route::post('/santa',[ChildController::class, 'store'])->name('apiSantaStore');
+Route::put('/santa/{id}',[ChildController::class, 'update'])->name('apiSantaUpdate');
